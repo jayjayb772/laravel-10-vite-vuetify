8b9c5cb3b166eb3d7749aca053cd96333bf82c88
Juniper Bender <jayjayb100@msn.com>
2023-03-27 12:38:43 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify'
+import Components from 'unplugin-vue-components/vite';
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        vuetify({ autoImport: true }),
+    ],
+});
